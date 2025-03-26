6e9530275928d4102aa503b0b446d00984b692b0
BakaHatake <aanush748@gmail.com>
2025-03-26 23:43:05 +0530

    Update some.py

--- some.py
+++ some.py
@@ -1,4 +1,6 @@
-import telebot
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, CallbackQueryHandler, ContextTypes
+
 import random
 
 token="7610705253:AAGVc7Yy-uhBRAq3IESkbDxh4rdhVzZ6OHo"
